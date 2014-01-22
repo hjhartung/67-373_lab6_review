67ef132b4286814530734a75c61078d2243282bf
Harris Hartung <hjh@andrew.cmu.edu>
2014-01-22 00:01:26 -0500

    added date validations to books model

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,6 +11,9 @@ class Book < ActiveRecord::Base
   
   # Date validations using validates_timeliness gem
   # see https://github.com/adzap/validates_timeliness for documentation
+  validates_date :proposal_date, :on_or_before => :today
+  validates_date :contract_date, :on_or_before => :today, :after => :proposal_date, allow_blank: true
+  validates_date :publish_date, :on_or_before => :today, :after => :contract_date, allow_blank: true
 
 
 
